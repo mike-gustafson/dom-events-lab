bb61f8e1603d8879144304aebd29de488f964eb5
Mike Gustafson <gustafson.m@gmail.com>
2024-10-17 21:43:21 -0700

    was working, then all of a sudden BAM I think im done??? idk, I'll come back through and clean it up later.  I'm going to be shocked if I can't break the code tomorrow night

--- app.js
+++ app.js
@@ -1,9 +1,86 @@
 /*-------------------------------- Constants --------------------------------*/
+const display = document.querySelector('.display')
+console.log(display)
 
+const numberButtons = document.querySelectorAll('.button.number')
+const operatorButtons = document.querySelectorAll('.button.operator')
+const equalsButton = document.querySelector('.button.equals')
 /*-------------------------------- Variables --------------------------------*/
+let numArray = []
+let newNum
+let num1 = 0
+let num2 = 0
+
+let operator = ''
+
+let result = 0
+display.innerText = result
 
 /*------------------------ Cached Element References ------------------------*/
 
+
+
 /*----------------------------- Event Listeners -----------------------------*/
 
+
+
 /*-------------------------------- Functions --------------------------------*/
+
+const enterNumber = (event) => {
+    numArray.push(event.target.innerText)
+    console.log(numArray)
+    newNum = Number(numArray.join(''))
+    console.log(newNum)
+    display.innerText = newNum
+}
+
+const preformOperation = (event) => {
+    num1 = newNum
+    numArray = []
+    operator = event.target.innerText
+    switch (operator) {
+        case 'C':
+            numArray = [] 
+            num1 = 0
+            num2 = 0
+            result = 0
+            display.innerText = result
+            console.log('num1:', num1, 'num2:',num2, 'numArray:',numArray)
+            break
+    }
+    console.log (num1, operator, num2)
+}
+
+const preformEquals = (event) => {
+    num2 = newNum
+    numArray = []
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2
+            break
+        case '-':
+            result = num1 - num2
+            break
+        case '*':
+            result = num1 * num2
+            break
+        case '/':
+            result = num1 / num2
+            break
+    }
+    display.innerText = result
+    newNum = result
+    num2 = 0
+
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', enterNumber)
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', preformOperation)
+})
+
+equalsButton.addEventListener('click', preformEquals)
