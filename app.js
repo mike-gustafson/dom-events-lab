476159852f236981063e0198f3c302a8f9f10f1e
Mike Gustafson <gustafson.m@gmail.com>
2024-10-23 18:08:01 -0700

    remove multiple 'querySelectorAll' and use logic in event listeners to call functions.  This should save some resources

--- app.js
+++ app.js
@@ -1,63 +1,67 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
-let numArray = []
-let newNum
-let num1 = 0
-let num2 = 0
-let operator = ''
-let result = 0
+let numArray = [];
+let newNum;
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+let result = 0;
 
 /*------------------------ Cached Element References ------------------------*/
-const operatorButtons = document.querySelectorAll('.button.operator')
-const numberButtons = document.querySelectorAll('.button.number')
-const equalsButton = document.querySelector('.button.equals')
-const display = document.querySelector('.display')
-display.innerText = result
+const allButtons = document.querySelectorAll('.button');
+const display = document.querySelector('.display');
+display.innerText = result;
 
 /*----------------------------- Event Listeners -----------------------------*/
-operatorButtons.forEach(button => {button.addEventListener('click', preformOperation) })
-numberButtons.forEach(button => { button.addEventListener('click', enterNumber) })
-equalsButton.addEventListener('click', preformEquals)
+allButtons.forEach(button => {
+    if (button.classList.contains('number')) {
+        button.addEventListener('click', enterNumber);
+    } else if (button.classList.contains('operator')) {
+        button.addEventListener('click', preformOperation);
+    } else if (button.classList.contains('equals')) {
+        button.addEventListener('click', preformEquals);
+    }
+});
 
 /*-------------------------------- Functions --------------------------------*/
 function preformOperation(event) {
-    num1 = newNum
-    numArray = []
-    operator = event.target.innerText
+    num1 = newNum;
+    numArray = [];
+    operator = event.target.innerText;
     if (operator === 'C' ) {
-            numArray = [] 
-            num1 = 0
-            num2 = 0
-            result = 0
-            display.innerText = result
+            numArray = [];
+            num1 = 0;
+            num2 = 0;
+            result = 0;
+            display.innerText = result;
     }
 }
 
 function enterNumber(event) {
-    numArray.push(event.target.innerText)
-    newNum = Number(numArray.join(''))
-    display.innerText = newNum
+    numArray.push(event.target.innerText);
+    newNum = Number(numArray.join(''));
+    display.innerText = newNum;
 }
 
 function preformEquals() {
-    num2 = newNum
-    numArray = []
+    num2 = newNum;
+    numArray = [];
     switch (operator) {
         case '+':
-            result = num1 + num2
-            break
+            result = num1 + num2;
+            break;
         case '-':
-            result = num1 - num2
-            break
+            result = num1 - num2;
+            break;
         case '*':
-            result = num1 * num2
-            break
+            result = num1 * num2;
+            break;
         case '/':
-            result = num1 / num2
-            break
+            result = num1 / num2;
+            break;
     }
-    display.innerText = result
-    newNum = result
-    num2 = 0
+    display.innerText = result;
+    newNum = result;
+    num2 = 0;
 }
